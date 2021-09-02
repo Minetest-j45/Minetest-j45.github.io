e1f5eb69e232447d75864279ce662a20a177a31b
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2021-09-02 17:41:30 +0100

    searching blogs

--- blog/app.js
+++ blog/app.js
@@ -6,6 +6,23 @@ async function GetBlogs() {
 
 	const myTable = data.split('\n').slice(1);
 
+	document.getElementsByClassName('blogs').item(0).replaceChildren();
+
+	var neDiv=document.createElement('Div');
+	neDiv.className = 'blog';
+	neDiv.id = "Title";
+	document.getElementsByClassName('blogs').item(0).appendChild(neDiv);
+
+	var newLine = document.createElement('br');
+ 	neDiv.appendChild(newLine);
+	
+	var bt = document.createElement("bigtext");
+	bt.innerHTML = "My Posts:";
+	neDiv.appendChild(bt)
+
+	var newLine = document.createElement('br');
+ 	neDiv.appendChild(newLine);
+
 	for (let iRow = myTable.length-1; iRow >= 0; iRow--) {
 
 		const row = myTable[iRow];
@@ -17,18 +34,17 @@ async function GetBlogs() {
 			columns[1],
 			columns[2]
 		);
-	}
+	};
 
-}
+};
 
 function createPostTitle(Title,Date,Text) {
-
 	//Blog Block
 	var newDiv=document.createElement('Div');
 	newDiv.className = 'blog';
 	newDiv.id = Title.replaceAll(" ", "_");
 	document.getElementsByClassName('blogs').item(0).appendChild(newDiv);
-	
+
 	var newLine = document.createElement('br');
  	newDiv.appendChild(newLine);
 
@@ -39,7 +55,7 @@ function createPostTitle(Title,Date,Text) {
 		makePageSpecificBlog(Title,Date,Text);
 	};
 	newDiv.appendChild(btn);
-}
+};
 
 function makePageSpecificBlog(Title,Date,Text) {
 	//tip
@@ -96,4 +112,76 @@ function makePageSpecificBlog(Title,Date,Text) {
 	
 	var newLine = document.createElement('br');
  	nDiv.appendChild(newLine);
-}
+};
+
+async function search() {
+	var input = document.getElementById("myInput");
+
+	//filter words to lower case
+	var filter = input.value.toLowerCase();
+	if (filter == "") {
+		GetBlogs();
+		return;
+	};
+	var filters = filter.split(" ");
+
+	var toDisplay= [];
+
+	const Blogs = await fetch('https://j1233.minetest.land/blog/blogs.csv');
+	const data = await Blogs.text();
+
+	const myTable = data.split('\n').slice(1);
+
+	//for every row (post)
+	for (let iRow = myTable.length-1; iRow >= 0; iRow--) {
+		const row = myTable[iRow];
+
+		const columns = row.split('|');
+
+		//for every column in the row (string)
+		for (let iCol = 0; iCol < columns.length; iCol++) {
+
+			//for each word in filter
+			for (let fRow = 0; fRow < myTable.length; fRow++) {
+				//make the blog string lower case
+				var lowCol = columns[iCol].toLowerCase();
+				var fil = filters[fRow];
+
+				//if search success and not already in todisplay
+				if (lowCol.includes(fil) && !toDisplay.includes(myTable[iRow])) {
+					toDisplay.push(myTable[iRow]);
+				};
+			};
+		};
+	};
+
+	document.getElementsByClassName('blogs').item(0).replaceChildren();
+
+	var neDiv=document.createElement('Div');
+	neDiv.className = 'blog';
+	neDiv.id = "Title";
+	document.getElementsByClassName('blogs').item(0).appendChild(neDiv);
+
+	var newLine = document.createElement('br');
+ 	neDiv.appendChild(newLine);
+	
+	var bt = document.createElement("bigtext");
+	bt.innerHTML = "My Posts:";
+	neDiv.appendChild(bt)
+
+	var newLine = document.createElement('br');
+ 	neDiv.appendChild(newLine);
+
+	for (let nRow = 0; nRow < toDisplay.length; nRow++) {
+
+		const rrow = toDisplay[nRow];
+
+		const ccolumns = rrow.split('|');
+
+		createPostTitle(	
+			ccolumns[0],
+			ccolumns[1],
+			ccolumns[2]
+		);
+	};
+};
