a70973c711011132773ac34b5386995ca18bf60c
Minetest-j45 <55553015+Minetest-j45@users.noreply.github.com>
2022-04-02 15:48:09 +0100

    mobile friendly

--- nav.js
+++ nav.js
@@ -2,13 +2,24 @@ async function AddNav() {
     var nav = document.createElement('div');
     nav.setAttribute('id', 'nav');
 
-    var percentage = 6; // % 
+    var percentage = 6;
     var screenWidth = window.screen.width;
     var wpixels = Math.round((percentage / 100) * screenWidth);
     var img = new Image(wpixels, wpixels);
     img.src = 'https://j1233.minetest.land/images/j45withname.png';
     img.style = 'image-rendering: pixelated; image-rendering: crisp-edges;';
     nav.appendChild(img);
+
+    var checkbox = document.createElement('input');
+    checkbox.setAttribute('type', 'checkbox');
+    checkbox.setAttribute('id', 'checkbox_toggle');
+    nav.appendChild(checkbox);
+
+    var burger = document.createElement('label');
+    burger.setAttribute('for', 'checkbox_toggle');
+    burger.setAttribute('class', 'burger');
+    burger.innerHTML = '&#9776;';
+    nav.appendChild(burger);
 
     var homeLink = document.createElement('a');
     homeLink.href = 'https://j1233.minetest.land/#nav';
